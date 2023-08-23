829cf670e41ba062c7d9cda91e0fb0c9ed9fd2bd
jaafar48 <125685075+jaafar48@users.noreply.github.com>
2023-08-23 02:33:15 +0100

    add new way to Calculate flags.c

--- Calculate flags.c
+++ Calculate flags.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
+ * get_flags - Calculate active flags
+ * @format: Formatted string in way to print the arguments
  * @i: take a parameter.
  * Return: Flags:
  */
